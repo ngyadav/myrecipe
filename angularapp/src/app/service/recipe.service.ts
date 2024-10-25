d1a44e1ceafb5d760a3bb1645cadbf77ee870e1d
ngyadav <118537866+ngyadav@users.noreply.github.com>
2024-10-25 23:21:03 +0530

    Update recipe.service.ts

--- angularapp/src/app/service/recipe.service.ts
+++ angularapp/src/app/service/recipe.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Recipe } from '../model/recipe';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
